e6e98d8cc66c30457df0672c3a410a424d41a637
advokat977 <advokat@ugovor24.com>
2025-09-05 12:36:17 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com', // kasnije promijeni na pravi domen
-  output: 'server',            // omogućava Cloudflare Functions kada budu trebale
-  adapter: cloudflare(),
+  site: 'https://example.com',
+  output: 'static',
   integrations: [sitemap()]
 });
